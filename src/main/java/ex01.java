fb988030f0b254351d33e40ae5e992afceecf1df
ryanf <ryanfisher00@outlook.com>
2021-09-05 22:35:09 -0400

    first commit

---
+++ src/main/java/ex01.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Ryan Fisher
+ */
+
+/*pseudocode:
+include necessities for input
+prompt user for input
+return input in a string
+ */
+
+import java.util.Scanner;
+
+public class ex01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name= sc.nextLine();
+        System.out.printf("Hello, %s, nice to meet you!%n", name);
+    }
+}
